501a49549bc9147b1e85d2f1f455f5ab98a6fed0
Nicolas Guillemot <nlguillemot@gmail.com>
2014-03-17 15:26:38 -0700

    initial commit

---
+++ tmsimtest.lua
@@ -0,0 +1,31 @@
+local tmsim = require "tmsim"
+
+q0 = 's'
+delta = {
+    { { 's', '#' } , { 't', 'L' } },
+    { { 't', '0' } , { 'u', '#' } },
+    { { 'u', '#' } , { 'v', 'R' } },
+    { { 'v', '#' } , { 'v', '0' } },
+    { { 'v', '0' } , { 's', 'L' } },
+    { { 't', '1' } , { 'w', '#' } },
+    { { 'w', '#' } , { 'x', 'R' } },
+    { { 'x', '#' } , { 'x', '1' } },
+    { { 'x', '1' } , { 's', 'L' } },
+    { { 't', '#' } , { 'y', 'R' } },
+    { { 'y', '#' } , { 'z', 'R' } },
+    { { 'z', '0' } , { 'z', 'R' } },
+    { { 'z', '1' } , { 'z', 'R' } },
+    { { 'z', '#' } , { 'h', '#' } }
+}
+
+acceptor = tmsim.NewAcceptor(q0)
+
+tests = {
+    '001'
+}
+
+for _,test in ipairs(tests) do
+    local result = acceptor(test)
+    local message = result and 'accepted' or 'not accepted'
+    print(test .. ' was ' .. message)
+end
